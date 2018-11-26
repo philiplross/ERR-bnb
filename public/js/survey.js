6554fa5a6949a2d2b130a18ba4c785f07ea4055d
philiplross <philiplross@gmail.com>
2018-11-26 12:42:39 -0800

    update survey

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,5 +1,36 @@
-var budget;
+ var budget 
+= alert("what is your budget for this trip?");
+// entering in a value from $1 to $10,000= id value capture for the box then attaching a value to it
 
-var questionOne = alert("what is your budget for this trip?");
+var questionOne 
+= alert("What dates will this trip be happening?");
+// them entering the numbered dates from start to end like so 11/27/2018
+
+
+var questionTwo
+= alert("Do you like staying alone, or enjoy the livelyness of a bed and breakfast?");
+// alone or shared will be choices to filter a shared Room VS private room
+
+
+var questionThree
+= alert("Would you rather sit in the warm sun, or look at pretty seasons with a jacket?");
+// filter out what locations from california, midwest, and new york
+
+var questionFour
+= alert("when traveleing somewhere, whats the first thing you do when you get there?");
+// short description that looks for keywords like, EAT, NAP, DRIVE, SIGHT SEE
+
+
+var questionFive
+= alert("Would you say this trip you wanna be a foodie, or stick to a diet and get some excerise?");
+// foodie option is chicago, new york, and excerise is san fransico and denver
+
+var questionSix
+= alert("when you stay somewhere whats more inportant, artistic & history, or comnfort & ritz?");
+
+
+
+var questionSeven
+= alert("");
 
 
