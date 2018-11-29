1e49998982e590c8c430bfd6c62a33dd1835059f
Philip Ross <philiplross@gmail.com>
2018-11-28 18:13:35 -0800

    Update survey.js

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,41 +1,30 @@
-var questions = {
-    budget: { msg: "what is your budget for this trip?" }
-}
-
 var budget
-    = alert()
-// entering in a value from $1 to $10,000= id value capture for the box then attaching a value to it
-
-var questionOne
-    = alert("What dates will this trip be happening?");
 // them entering the numbered dates from start to end like so 11/27/2018
 
 
-var questionTwo
-    = alert("Do you like staying alone, or enjoy the livelyness of a bed and breakfast?");
+var questionTwo = "Do you like staying alone, or enjoy the livelyness of a bed and breakfast?"
 // alone or shared will be choices to filter a shared Room VS private room
 
 
 var questionThree
-    = alert("Would you rather sit in the warm sun, or look at pretty seasons with a jacket?");
+    = alert("Would you rather sit in the warm sun, or look at pretty seasons with a jacket?")
 // filter out what locations from california, midwest, and new york
 
 var questionFour
-    = alert("when traveleing somewhere, whats the first thing you do when you get there?");
+    = alert("when traveleing somewhere, whats the first thing you do when you get there?")
 // short description that looks for keywords like, EAT, NAP, DRIVE, SIGHT SEE
 
 
 var questionFive
-    = alert("Would you say this trip you wanna be a foodie, or stick to a diet and get some excerise?");
+    = alert("Would you say this trip you wanna be a foodie, or stick to a diet and get some excerise?")
 // foodie option is chicago, new york, and excerise is san fransico and denver
 
 var questionSix
-    = alert("when you stay somewhere whats more inportant, artistic & history, or comnfort & ritz?");
-
+    = alert("when you stay somewhere whats more inportant, artistic & history, or comnfort & ritz?")
 
 
 var questionSeven
-    = console.log("to be determined");
+    = console.log("to be determined")
 
 var questionEight =
 console.log("are you a big sports fan and would see a local game?")
@@ -43,12 +32,14 @@ console.log("are you a big sports fan and would see a local game?")
 
 var questionNine =
 console.log("if you were able to endulge would you rather drink a nice cocktail, or smoke a smooth rolled joint?")
-// San fran and denver for smoking, new york and chicago on drinking
+// San fran and denver for smoking, new york and chicago on drinking;
 
 
 var questionTen =
-console.log("to be determined")
+console.log("to be determined");
 
+var questions = {
+    budget: { msg: "what is your budget for this trip?" }
+}
 
-
-module.exports = questions;
+module.exports = questions
