34bcaf42f179d7767b64f6264455bfba1937630a
Captain Drew <amwest21@gmail.com>
2018-12-05 18:20:51 -0800

    update tweets

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,17 @@ var db = require("../models");
 
 var testQuestions = require("../test-data/questions.js")
 
+//Tweet Tweet Make This Global aka first.
+var Twitter = require('twitter');
+//Make this global aka first.
+var client = new Twitter({
+    consumer_key: 'CSJvSoGmLJt8yie3a7g8RMfPM',
+    consumer_secret: 'ZwDoLhQ6hTevtcxSvXPWnn6ZyXQm2f3gBoRL95ofWYyxB5cFLE',
+    access_token_key: '1018728385-nIiGtiNBNFFWFQFAONdlGWiKhwtNLHKDzClryxp',
+    access_token_secret: '7ObeDoMcfHCZ2NlRd0EoXtkJl9iP3HdxzNYuBHafZYYwM'
+});
+
+
 module.exports = function (app) {
   // Get all examples
   app.get("/api/survey", function (req, res) {
@@ -27,5 +38,31 @@ module.exports = function (app) {
   // });
   app.get("/api/lisiting", function (req, res) {
     res.json({ message: "" })
-  })
+  });
+
+  app.get('/tweet', function(req, res) {
+    var neighborhood = req.params.neighborhood;
+    client.get('search/tweets.json?q=nasa', {q: neighborhood}, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+        return res.json(tweets);
+    });
+  });
+
+
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
